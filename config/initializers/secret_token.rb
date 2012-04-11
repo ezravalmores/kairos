0eebe3a13139382843a3a24154caf43a342155d1
ezravalmores <evalmores@nazarene.org>
2012-04-11 10:15:06 -0500

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ncm1Kairos::Application.config.secret_token = '6e72bef84de80311e0f2161b4366d748b2ec05f7ab856deac67401063a2d84cf0ea41f2a1897b79acd7ebe6116e7b72796f6d658f808b4b8aebe07b78f7fab31'
