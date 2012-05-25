2f0b3d02c3b60889c8ca7ab5b0b65634ec01b37c
ezravalmores <evalmores@nazarene.org>
2012-05-25 13:30:05 -0500

    Added a approvals method.

--- app/mailers/kairos1_mailer.rb
+++ app/mailers/kairos1_mailer.rb
@@ -4,6 +4,21 @@ class Kairos1Mailer < ActionMailer::Base
   def welcome_message(person)
     @person = person
 
-    mail(:to => person.email_address, :from => "Kairos <reports@ncm.org>", :subject => "Welcome to NCM Child Sponsorship powered by Sprout")
+    mail(:to => person.email_address, :from => "Kairos <reports@ncm.org>", :subject => "Welcome to Kairos")
   end
+  
+  def send_to_selected(person)
+    @person = person
+
+    mail(:to => person.email_address, :from => "Kairos <reports@ncm.org>", :subject => "Tasks Approvals")
+  end 
+  
+  def send_approvals(person,submitted_by)
+    @submitted_by = submitted_by
+    @to = person
+     #for person in people 
+        @person = person
+        mail(:to => person.email_address, :from => "Kairos <reports@ncm.org>", :subject => "Tasks Approvals")
+    #end    
+  end   
 end
