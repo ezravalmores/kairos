31f9797adbb5f8ec1f5237ed29128527bc106b81
ezravalmores <evalmores@nazarene.org>
2012-05-29 16:32:30 -0500

    Added a date in send approvals.

--- app/mailers/kairos1_mailer.rb
+++ app/mailers/kairos1_mailer.rb
@@ -4,21 +4,22 @@ class Kairos1Mailer < ActionMailer::Base
   def welcome_message(person)
     @person = person
 
-    mail(:to => person.email_address, :from => "Kairos <reports@ncm.org>", :subject => "Welcome to Kairos")
+    mail(:to => person.email_address, :from => "Kairos <no-reply@ncm.org>", :subject => "Welcome to Kairos")
   end
   
   def send_to_selected(person)
     @person = person
 
-    mail(:to => person.email_address, :from => "Kairos <reports@ncm.org>", :subject => "Tasks Approvals")
+    mail(:to => person.email_address, :from => "Kairos <no-reply@ncm.org>", :subject => "Tasks Approvals")
   end 
   
-  def send_approvals(person,submitted_by)
+  def send_approvals(person,submitted_by,shift_date)
     @submitted_by = submitted_by
     @to = person
+    @shift_date = shift_date
      #for person in people 
         @person = person
-        mail(:to => person.email_address, :from => "Kairos <reports@ncm.org>", :subject => "Tasks Approvals")
+        mail(:to => person.email_address, :from => "Kairos <no-reply@ncm.org>", :subject => "Tasks Approvals")
     #end    
   end   
 end
