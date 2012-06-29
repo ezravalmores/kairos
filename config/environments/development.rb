a3eec30771c86d680169bcacee366dc431b4a6e7
ezravalmores <evalmores@nazarene.org>
2012-06-29 13:49:37 -0500

    Modified mailer settings.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,18 @@ Ncm1Kairos::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :domain => '10.7.203.241',
+    :user_name => 'ezravalmores@gmail.com',
+    :password => 'ezda0830'
+  }
+
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   :location => '/usr/sbin/sendmail',
