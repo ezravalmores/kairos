3e047abde983c531c8e114bf6d58865761b861c7
ezravalmores <evalmores@nazarene.org>
2012-07-18 10:35:40 -0500

    Added methods.

--- app/models/specific_activity.rb
+++ app/models/specific_activity.rb
@@ -4,4 +4,28 @@ class SpecificActivity < ActiveRecord::Base
   has_many :person_times
   
   validates_presence_of :name, :activity
+  
+  scope :active, where(:is_active => true)
+  
+  #instance method
+  def deactivate
+    update_attributes(:is_active => false)    
+  end
+  
+  def activate
+    update_attributes(:is_active => true)    
+  end
+  
+  #class methods
+  def self.deactivate_specific_activities(specific_activities)
+    specific_activities.each do |sa|
+      sa.deactivate  
+    end        
+  end  
+  
+  def self.activate_specific_activities(specific_activities)
+    specific_activities.each do |sa|
+      sa.activate  
+    end        
+  end   
 end
