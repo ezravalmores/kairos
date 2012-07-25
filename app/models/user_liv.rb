63d9148ab92c72af88a2cdce3a3ab7a5adf25cc6
ezravalmores <evalmores@nazarene.org>
2012-07-25 15:42:45 -0500

    Made some changes and added realtionship

--- app/models/user_liv.rb
+++ app/models/user_liv.rb
@@ -1,6 +1,7 @@
 class UserLiv < ActiveRecord::Base
   belongs_to :leave_type
   belongs_to :person
+  has_one :activity_log, :as => :activity_log_type, :dependent => :destroy
   
   #scopes
   scope :is_submitted, where(:is_submitted => true)
