2162f8510ff7b3b4d729b3276397d2267ca30473
ezravalmores <evalmores@nazarene.org>
2012-07-30 15:37:14 -0500

    Modified.

--- app/models/ncmm_activity.rb
+++ app/models/ncmm_activity.rb
@@ -1,4 +1,5 @@
 class NcmmActivity < ActiveRecord::Base
   belongs_to :person
   has_one :activity_log, :as => :activity_log_type, :dependent => :destroy
+  has_one :notification, :as => :notification_log_type, :dependent => :destroy
 end  
