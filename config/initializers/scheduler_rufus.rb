f4edc5e193614987cfc761fd8f6c2b84ffda9b1d
ezravalmores <evalmores@nazarene.org>
2012-07-31 16:14:30 -0500

    Modified.

--- config/initializers/scheduler_rufus.rb
+++ config/initializers/scheduler_rufus.rb
@@ -15,20 +15,21 @@ scheduler.every '2m' do
       people.each do |person|
         Notification.create!(:person_id => person.id,:activity_log_id => activity.id, :notification_log_type_type => activity.activity_log_type_type, :notification_log_type_id => activity.activity_log_type_id)
       end
+    #Notification.create!(:person_id => activity.person_id,:activity_log_id => activity.id, :notification_log_type_type => activity.activity_log_type_type, :notification_log_type_id => activity.activity_log_type_id)  
     activity.has_created_notifications = true
     activity.save!  
     end
   end
   
-  activities = ActivityLog.check_coming_activity_individual.people_involved_null
+ # activities = ActivityLog.check_coming_activity_individual.people_involved_null
   
-  if activities.count > 0
-    activities.each do |activity|
-      Notification.create!(:person_id => activity.person_id,:activity_log_id => activity.id, :notification_log_type_type => activity.activity_log_type_type, :notification_log_type_id => activity.activity_log_type_id)
+  #if activities.count > 0
+   # activities.each do |activity|
+    #  Notification.create!(:person_id => activity.person_id,:activity_log_id => activity.id, :notification_log_type_type => activity.activity_log_type_type, :notification_log_type_id => activity.activity_log_type_id)
 
-      activity.has_created_notifications = true
-      activity.save!  
-    end
-  end
+     # activity.has_created_notifications = true
+    #  activity.save!  
+    #end
+  #end
 end
 
