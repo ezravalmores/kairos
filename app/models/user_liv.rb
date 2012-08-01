3dcdf265d28925945121ef21f1b402546b9064c4
ezravalmores <evalmores@nazarene.org>
2012-08-02 00:13:39 +0800

    Modified.

--- app/models/user_liv.rb
+++ app/models/user_liv.rb
@@ -1,7 +1,7 @@
 class UserLiv < ActiveRecord::Base
   belongs_to :leave_type
   belongs_to :person
-  has_one :activity_log, :as => :notification_log_type, :dependent => :destroy
+  has_one :activity_log, :as => :activity_log_type, :dependent => :destroy
   has_one :notification, :as => :notification_log_type, :dependent => :destroy
   
   #scopes
