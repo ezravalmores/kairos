5c30126134d9657e6479f31f360767645aae0608
ezravalmores <evalmores@nazarene.org>
2012-10-03 00:28:28 +0800

    Modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 module ApplicationHelper
+  def flash_notice(message, type)
+    # note: you must have a div with id='notices' or rename the div appended to below with your element which
+    # is the container for the flash messages
+    flash.discard
+    notice = <<-EOF
+      $("html, body").animate({ scrollTop: 0 }, 1000);
+      $('div#for_flash').replaceWith("<div id='for_flash'></div>");
+      $('div#for_flash').prepend("<div class='#{type}'><h3>#{message}</h3></div>");
+      $('#for_flash').delay(500).slideDown('slow').delay('8000').slideUp('slow'); 
+    EOF
+    notice.html_safe
+  end
+  
   def add_activity_link(name)
     link_to_function name, :id => "add-activity-link", :class => "red" do |page|
       page.show 'add-activity-form'
