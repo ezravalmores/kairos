b82ac4902bdfb7ac851620ba2aa9530af1561420
Ezra Valmores <evalmores@nazarene.org>
2013-12-07 00:00:22 +0800

    modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,8 @@ Ncm1Kairos::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.active_record.default_timezone = :local
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
